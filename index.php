99dcdc59aec4f3f5b082bdc9db22c59ce7d75ba7
Jake Ferrante <ja032964@sdesosiweb1.sdes.ucf.edu>
2015-02-23 17:32:36 -0500

    Cleans up general CSS, making theme more spacey and metro-like. Adds no-fill buttons for registration section. Defines seveal new sections on index.php (general information, when and where, registration, etc.). Changes to default typography and header defaults. Modifies already existing footer to better fit the theme but changing its background to match the navigation. More, less significant CSS changes...

--- index.php
+++ index.php
@@ -11,126 +11,99 @@
    <!--  <?php // Include registration form
     //include_once( 'partials/register.php' ); ?> -->
 
-    <section id="about" class="header-caption" style="background-color: rgba(122, 218, 250, 1)">
-        <!-- <h2>Light The Knight 5K</h2> -->
-        <h4>Get your run on! Join us for the first ever Light The Knight 5k!</h1>
-        <!-- <h3>What's going on?</h4> -->
+    <section id="about" class="header-caption" style="background-color: rgb(122, 218, 250)">
+        <h4>Get your run on! Join us for the first ever Light The Knight 5k!</h4>
     </section>
 
     <section class="content">
-        
         <article class='info'>
-            <h3>Join us for the annual Light The Knight Run!
-<!--             <p>It happened again: A chemistry experiment gone wrong has resulted in the virus that is beginning the Zombie Apocalypse, and it is beginning at UCF. Our campus is being overrun by the living-dead and they are very, very hungry.</p>
-            <ul>
-                <li>Registration Dates: 08/19/2014 - 11/9/2014 </li>
-                <li>Race date: 11/13/2014</li>
-                <li>Start time: 8:00 PM</li>
-            </ul> -->
+            <h3>General Information</h3>
+            <p>It happened again: A chemistry experiment gone wrong has resulted in the virus that is beginning the Zombie Apocalypse, and it is beginning at UCF. Our campus is being overrun by the living-dead and they are very, very hungry.</p>
         </article>
-       <!--  <article class='survive'>
-            <h4>How do you survive?</h4>
-            <p>The two (2) flags attached to your belt represent your life. That's two lives you have to get yourself across campus into the safety zone. We have mapped out the safest path, but you have to move fast. Obstacles may stand in your way and there's a chance zombies have broken through our barricades - stay aware. We have been able to set up a first aid station along the path, if you lose a life it's possible our medics can help.</p>
-            <p>Your peers, intuition and agility are all that can help you now. Make it to the safe zone with at least one flag intact and you have survived - if not, you'll be missed.</p>
+
+        <article class="col-wrapper">
+            <div class="col-md-6">
+                <h3>When and Where?</h3>
+                <p>Date: <span>3/17/15</span></p>
+                <p>Show up: <span>5PM - 6:45PM</span></p>
+                <p>Race Duration: <span>7PM - 11PM</span></p>
+                <p>Your peers, intuition and agility are all that can help you now. Make it to the safe zone with at least one flag intact and you have survived - if not, you'll be missed.</p>
+            </div>
+            <div class="col-md-6">
+                <i class="fa fa-mail"></i>
+                <!--<h3>When and Where?</h3>
+                <p>Date: <span>3/17/15</span></p>
+                <p>Show up: <span>5PM - 6:45PM</span></p>
+                <p>Race Duration: <span>7PM - 11PM</span></p>
+                <p>Your peers, intuition and agility are all that can help you now. Make it to the safe zone with at least one flag intact and you have survived - if not, you'll be missed.</p> -->
+            </div>
         </article>
+    </section>
+
+    
+    <section id="register" class="header-caption" style="background-color: rgb(240, 78, 236)">
+        <h4>RUNNING. GLOWSTICKS. FUN. WHAT MORE COULD YOU WANT?</h4>
+    </section>
+
+
+    <section class="no-padding">
+        <article style="background-color:rgb(237, 237, 237);" class="no-padding">
+            <h3 style="padding-top:2em;">Register Today!</h2>
+            <div class="registration-button-wrapper">
+                <a class="button no-fill runner">Runner Registration<i class="fa fa-arrow-right"></i></a>
+                <a class="button no-fill volunteer">Volunteer Registration<i class="fa fa-arrow-right"></i></a>
+            </div>
+        </article>
+    </section>
+
+
+<!--     <section class="no-padding">
+        <article style="background-color:rgb(237, 237, 237);" class="no-padding">
+            <div class="half-wrap">
+                <h5 style="padding-top:2em;">Register!</h4>
+                <ul>
+                    <li>Start Line: Memory Mall</li>
+                    <li>Finish Line: Memory Mall</li>
+                </ul>
+                <p>What to bring: The clothes on your body and a UCF ID. The UCF Campus Activities Board is not responsible for the loss of any items. Please leave precious items at home, or in your car, and assume all responsibility.</p>
+            </div>
+            <div class="half-wrap">
+            </div>
+        </article>
+    </section> -->
+<!-- 
+    <section class="content">
         <article class='location'>
-            <h4>Location</h4>
+            <h3>Location</h3>
             <ul>
                 <li>Start Line: Memory Mall</li>
                 <li>Finish Line: Memory Mall</li>
             </ul>
             <p>What to bring: The clothes on your body and a UCF ID. The UCF Campus Activities Board is not responsible for the loss of any items. Please leave precious items at home, or in your car, and assume all responsibility.</p>
-            <p>Race Map:</p>
-            <p class="map">
-                <img src="img/map.jpg" alt="race-map">
-            </p> 
+
         </article> 
         <article class='packet'>
-            <h4>Packet Pickup</h4>
+            <h3>Packet Pickup</h3>
             <p>Packet Distribution, including your bib number and information for the race, will be available on Monday (11/10) and Tuesday (11/11) from 5:00pm - 8:00pm in the Arena Lobby.</p>
         </article>
         <article class='party'>
-            <h4>Survivor Party!</h4>
+            <h3>Survivor Party!</h3>
             <p>What better way to celebrate your survival - or demise - than with a giant party? DJs, snacks, and activities will be available! Take pictures to remember the occasion and make sure to strategize with the other survivors for the next time zombies overrun UCF.</p>
         </article>
         <article class='info'>
-            <h4>Spectators</h4>
+            <h3>Spectators</h3>
             <p>Spectators are welcome to watch at the start line or on Memory Mall (as the runners make their final attempt to safety). All spectators are welcome to attend the survivor party with their brave friends (or mourn the ones that they lost) at the Arena after the race!</p>
         </article>
         <article class='other'>
             <p>Please email <a href="mailto:cabevent@ucf.edu">cabevent@ucf.edu</a> or call <a href="tel:4078233294">(407) 823-3294</a> for any questions!</p>
         </article>
+    </section> -->
+
+
+
+    <section id="about" class="header-caption" style="background-color: rgba(122, 218, 250, 1)">
+        <h4>Become part of the race! Register as a runner or volunteer today!</h4>
     </section>
 
-
-    <section id='zombies'>
-        <h3>Zombie Knights 5K: Information for Zombies</h3>
-        <article class='intro'>
-            <p>Runner space already full? Not sure you could survive? No worries, there is a place for you.</p>
-            <p>Sign up to get involved with this event. We will place you anywhere from the first aid station to a race official. You must be available for informational meeting on Monday (11/10) or Tuesday (11/11) either 5:00pm - 6:00pm or 6:00pm - 7:00pm at the Arena Lobby.</p>
-        </article>
-        <article class='info'>
-            <h4>What's going on?</h4>
-            <p>It happened again: A chemistry experiment gone wrong has resulted in the virus that is beginning the Zombie Apocalypse, and it is beginning at UCF. Our campus is being overrun by the living-dead and they are very, very hungry.</p>
-            <ul>
-                <li>Registration Dates: 08/19/2014 - 11/9/2014 </li>
-                <li>Race date: 11/13/2014</li>
-                <li>Start time: 8:00 PM</li>
-            </ul>
-        </article>
-        <article class='location'>
-            <h4>Location:</h4>
-            <ul>
-                <li>Zombies will be placed at various locations around campus.</li>
-            </ul>
-            <p>What to bring: The clothes on your body and a UCF ID. The UCF Campus Activities Board is not responsible for the loss of any items. Please leave precious items at home, or in your car, and assume all responsibility.</p>
-            <p>Race Map:</p>
-             <p class="map">
-                <img src="img/map.jpg" alt="race-map">
-            </p> 
-        </article>
-        <article class='party'>
-            <h4>Survivor Party!</h4>
-            <p>What better way to celebrate the survival - or demise - of the runners than with a giant party? DJs, snacks, and activities will be available! Take pictures to remember the occasion and make sure to strategize with the other survivors for the next time zombies overrun UCF.</p>
-        </article>
-        <article class='other'>
-            <p>Please email <a href="mailto:cabevent@ucf.edu">cabevent@ucf.edu</a> or call <a href="tel:4078233294">(407) 823-3294</a> for any questions!</p>
-        </article>
-    </section>
-
-
-    <section id='volunteers'>
-        <h3>Zombie Knights 5K: Information for Volunteers</h3>
-        <article class='intro'>
-            <p>Event space already full? Not sure you could survive? Not dead enough to be a zombie? No worries, there is a place for you.</p>
-            <p>Sign up to get involved with this event. We will place you anywhere from the first aid station to a race official. You must be available for informational meeting on Monday (11/10) or Tuesday (11/11) either 5:00pm - 6:00pm or 6:00pm - 7:00pm at the Arena Lobby.</p>
-        </article>
-        <article class='info'>
-            <h4>What's going on?</h4>
-            <p>It happened again: A chemistry experiment gone wrong has resulted in the virus that is beginning the Zombie Apocalypse, and it is beginning at UCF. Our campus is being overrun by the living-dead and they are very, very hungry.</p>
-            <ul>
-                <li>Registration Dates: 08/19/2014 - 11/9/2014 </li>
-                <li>Race date: 11/13/2014</li>
-                <li>Start time: 8:00 PM</li>
-            </ul>
-        </article>
-        <article class='location'>
-            <h4>Location:</h4>
-            <ul>
-                <li>Volunteers will be placed at various locations around campus.</li>
-            </ul>
-            <p>What to bring: The clothes on your body and a UCF ID. The UCF Campus Activities Board is not responsible for the loss of any items. Please leave precious items at home, or in your car, and assume all responsibility.</p>
-            <p>Race Map:</p>
-            <p class="map">
-                <img src="img/map.jpg" alt="race-map">
-            </p>
-        </article>
-        <article class='party'>
-            <h4>Survivor Party!</h4>
-            <p>What better way to celebrate the survival - or demise - of the runners than with a giant party? DJs, snacks, and activities will be available! Take pictures to remember the occasion and make sure to strategize with the other survivors for the next time zombies overrun UCF.</p>
-        </article>
-        <article class='other'>
-            <p>Please email <a href="mailto:cabevent@ucf.edu">cabevent@ucf.edu</a> or call <a href="tel:4078233294">(407) 823-3294</a> for any questions!</p>
-        </article> -->
-    </section>
+  
 <?php get_footer(); ?>
