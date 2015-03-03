8371a150a0bc9c50d93d8111bd16ba51b8cf393f
Jake Ferrante <ja032964@sdesosiweb1.sdes.ucf.edu>
2015-03-03 17:33:08 -0500

    Adds new interface file to handle global variables (global-variables.php) and adds new registration completion partial to /partials

---
+++ partials/registration-complete.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Registration complete partial
+ *
+ * This will be displayed once someone has been registered for the
+ * event. This will be the confirmation page for registrants.
+ */
+include_once( 'functions.php' );
+get_header();
+?>
+
+
+	<section class="content">
+
+		<article class="form" id="form">
+			
+			<h3>Thank you for helping us make this possible!</h3>
+		
+		</article>
+	</section>
+
+<?php get_footer(); ?>
